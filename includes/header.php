72c21f1415b3e67f8c1d1429287bc07f8298d437
JosseLebas <101173864+JosseLebas@users.noreply.github.com>
2022-05-16 00:06:08 +0200

    mise ene page

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,21 @@
 <?php
-include_once("head.php")
+include_once("head.php");
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #31393c">
-  <a class="navbar-brand" href="index.php">Les Règles du jeu</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-gradient" style="background-color: #264653">
+  <a class="navbar-brand my-3 mx-3 text-white" href="index.php">Les Règles du jeu</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="objectifs.php">Objectifs</a>
+        <a class="nav-link text-white" href="objectifs.php">Objectifs</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="gestionProjet.php">Gestion de projet</a>
+        <a class="nav-link text-white" href="gestionProjet.php">Gestion de projet</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="etapes.php">Etapes du projet</a>
+        <a class="nav-link text-white" href="etapes.php">Etapes du projet</a>
       </li>
     </ul>
   </div>
