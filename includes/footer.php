113cb6ae210d66995b9856cb5af8623109aefee2
JosseLebas <101173864+JosseLebas@users.noreply.github.com>
2022-05-15 11:42:31 +0200

    footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">Fait par Josselin Lebas, Marie Leblanc et Hugo Tavard</p>
+    <ul class="list-inline">
+      <small><li class="list-inline-item"><a href="#">Haut de la page ↑</a></li></small>
+    </ul>
+</footer>
--- objectifs.php
+++ objectifs.php
@@ -20,4 +20,8 @@ Le projet Les Règles du Jeu a alors pour but de mettre en place une présentati
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
+
+<?php
+include_once("includes/footer.php");
+?>
 </html>
