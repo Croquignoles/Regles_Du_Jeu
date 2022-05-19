bbc0018369468f4f5b2cc839efe0520b4b6612ee
MarieLeblanc002 <101551141+MarieLeblanc002@users.noreply.github.com>
2022-05-19 09:01:59 +0200

    Ajout exemple regles

--- etat_de_art.php
+++ etat_de_art.php
@@ -8,10 +8,11 @@ include_once("includes/header.php");
         Comme première mission pour ce projet, nous nous étions fixé de faire des recherches sur ce qui existait déjà concernant la clarté des règles de jeux mais surtout sur les travaux permettant un divertissement pour le joueur. 
     </p>
     <p>
-        Concernant la clarté des règles, nous avons recherché les différents formats afin de partager les règles aux joueurs. Les créateurs optent toujours pour un format papier qui constitue le livret de règle. Mais ils peuvent également mettre en place une vidéo explicative (via YouTube). En plus de cela, nous retrouvons de nombreuses vidéos réalisées par les joueurs eux-mêmes.
+        Concernant la clarté des règles, nous avons recherché les différents formats servant à partager les règles aux joueurs. Les créateurs optent toujours pour un format papier qui constitue le livret de règle. Mais ils peuvent également mettre en place une vidéo explicative (via YouTube). En plus de cela, nous retrouvons de nombreuses vidéos réalisées par les joueurs eux-mêmes.
     </p>
     <p>
-        Les règles écrites se présentent souvent sous la même forme, très illustrées (avec le matériel du jeu) et partant du général vers le spécifique. Ces dernières commencent majoritairement par une explication globale de l’univers du jeu, suivi de la présentation du matériel et de la mise en place. Le reste du livret est ensuite propre au jeu mais certains mécanismes reviennent fréquemment. Il s’agit de la forte présence d'exemples accompagnés des illustrations à propos, ou encore d’encadrés qui ressortent visuellement et qui résument les idées les plus importantes évoquées plus haut dans le livret.
+        Les règles écrites se présentent souvent sous la même forme, très illustrées (avec le matériel du jeu) et partant du général vers le spécifique. Ces dernières commencent majoritairement par une explication globale de l’univers du jeu, suivi de la présentation du matériel et de la mise en place. Le reste du livret est ensuite propre au jeu mais certains mécanismes reviennent fréquemment. Il s’agit de la forte présence d'exemples accompagnés des illustrations à propos, ou encore d’encadrés qui ressortent visuellement et qui résument les idées les plus importantes évoquées plus haut dans le livret. <br/>
+        <a href="pdf/Table_Ronde.pdf" target="_blank"> Exemple d'un livret de règles </a>
     </p>
     <table>
         <td width="80%">
