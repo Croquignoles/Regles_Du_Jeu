c8d40aac00eedbb8bd112342ab53e086ead9518f
MarieLeblanc002 <101551141+MarieLeblanc002@users.noreply.github.com>
2022-05-20 11:09:52 +0200

    ajout images

--- observations.php
+++ observations.php
@@ -16,13 +16,16 @@ include_once("includes/header.php");
             <li>Les participants peuvent aussi ne jamais avoir été confrontés à Complots. Dans ce cas, nous leur demandons de lire les règles ensemble. Aucune consigne plus précise ne leur est donnée. Il est alors intéressant de prendre note de la méthode choisie par le groupe pour faire cela. Une fois les règles acquises, nous observerons, de la même manière que précédemment une partie. Nous pourrons noter les cas où les règles ont posé problème, et où elles ne sont pas respectées (volontairement ou non).</li>
         </ul>
         <p>Dans les deux cas, nous avons mis en place une grille d’observation qui permet de mettre en évidence les différents points qui nous semblent pertinents. Cette dernière à pour but de formaliser les résultats mais ne doit en aucun cas représenter un frein. Nous restons largement ouverts à des situations que nous n’avions pas envisagées.</p>
+        <img src="images/grille_observation.png" alt="Grille utilisée lors des observations" width=100%>
+        
         <h1 style="color: #f4a261">QUESTIONNAIRE</h1>
-        <p>Afin de pouvoir récupérer facilement les avis des joueurs sur la partie (et sur la lecture des règles quand elle a eu lieu) qu’ils viennent de réaliser, nous leur proposons un questionnaire (voir annexe). Cela a pour objectif de pouvoir récupérer le ressenti des joueurs sur la lecture des règles au point de vue divertissement. 
+        <p>Afin de pouvoir récupérer facilement les avis des joueurs sur la partie (et sur la lecture des règles quand elle a eu lieu) qu’ils viennent de réaliser, nous leur proposons un questionnaire. Cela a pour objectif de pouvoir récupérer le ressenti des joueurs sur la lecture des règles au point de vue divertissement. 
         <br/>
-        Ce questionnaire doit être bref (quelques questions) et se fera de manière écrite afin que tous les joueurs le réalisent en même temps, directement après la partie (pas de perte d'information). Il sera anonyme et il faudra rappeler au joueur que ses réponses ne seront pas jugées et nous seront dans tous les cas très utiles.
+        Ce questionnaire doit être bref (quelques questions) et se fera de manière écrite afin que tous les joueurs le réalisent en même temps, directement après la partie (pas de perte d'information). Il sera anonyme et il faudra rappeler au joueur que ses réponses ne seront pas jugées et nous seront dans tous les cas très utiles.<br/>
+        <a href="pdf/questionnaire_obs.pdf" target="_blank"> Questionnaire présenté suite à l'observation</a>
         <br/>
         Les réponses ont ensuite été analysées et confrontées entre elles pour en faire ressortir un modèle et nous permettre de mettre en place une maquette.</p>
-        <h1 style="color: #e76f51">RESULTATS</h1>
+        <h1 style="color: #e76f51">BILAN</h1>
         <p>Lors des différentes observations, nous avons préparé un planning afin que chaque observation soit supervisée par deux membres de notre équipe. Nous avons donc finalement pu observer 5 groupes ce qui fut amplement suffisant étant donné que chaque groupe comportait au moins 4 personnes. Notons aussi qu’aucune des personnes observées ne connaissait les règles du jeu ce qui nous a permis de suivre l’apprentissage avec attention. NNotre responsable de projet s’est également joint à nous pour la dernière observation.</p>
 
 
