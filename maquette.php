6e16c8b79db8866cbba127a5a1ce9515f4611c94
MarieLeblanc002 <101551141+MarieLeblanc002@users.noreply.github.com>
2022-05-20 11:48:22 +0200

    Ajout mise en évidence

--- maquette.php
+++ maquette.php
@@ -36,7 +36,8 @@ include_once("includes/header.php");
             </p>
         </td>
 </table>
-<a href="pdf/nos_regles.pdf" target="_blank"> Notre maquette </a>
+<a href="pdf/nos_regles.pdf" target="_blank"> Notre maquette </a> <br/>
+<a href="pdf/modifications.pdf" target="_blank"> Mise en évidence des modifications </a>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </div>
 </body>
